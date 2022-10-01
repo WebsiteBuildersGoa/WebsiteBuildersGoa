55a58f9c99eeee3ced5ccd3dc0d4b68c4d5574b8
WebsiteBuildersGoa <114090397+WebsiteBuildersGoa@users.noreply.github.com>
2022-10-01 12:58:04 +0545

    Update herom.js

--- src/herom.js
+++ src/herom.js
@@ -1,11 +1,9 @@
 import {
-  Button,
   Flex,
   Heading,
   Image,
   Stack,
   Text,
-  Link,
   useBreakpointValue,
 } from '@chakra-ui/react';
 
@@ -26,56 +24,24 @@ export default function SplitScreen() {
                 bottom: 1,
                 left: 0,
                 color:'blue'
-                // // bg: 'blue.400',
-                // bgGradient:"linear(to-br,  white, #15aabf)", bgClip:"text"
-                // // zIndex: -1,
               }}>
                GET A PROFESSIONAL
             </Text>
             <br />{' '}
-            <Text  bgGradient="linear(to-br,  white, #15aabf)" bgClip="text" as={'span'}>
+            <Text  bgGradient="linear(to-br,  white, #26f705)" bgClip="text" as={'span'}>
             WEBSITE FOR YOUR BUSINESS
             </Text>{' '}
           </Heading>
           <Text justifyContent="space-between" textAlign="justify"mb="40px"fontSize={{ base: 'md', lg: 'lg' }} color={'white'}>
-        WebsiteBuilder Goa is a digital marketing and a website designing company with highly qualified professionals. Our team is highly skilled and experienced in understanding the general consumer behaviours and moderating the UX of the website according to the purchase behaviours. Your website will be carefully tailored with precisely following our proprietary 162 steps procedure which results in extremely high quality ERROR FREE WEBSITES which generates results.
+          WebsiteBuilders Goa is a digital marketing and a website designing company with highly qualified professionals. Our team is highly skilled and experienced in understanding the general consumer behaviours and moderating the UX of the website according to the purchase behaviours.
           </Text>
           <Stack direction={{ base: 'column', md: 'row' }} spacing={6}>
-          {/* <Button>
-                    <Link
-                    href="signup"
-                    color="white"
-                    variant="solid"
-                    size="lg"
-                    rounded="md"
-                    mb={{ base: 2, sm: 0 }}
-                    lineHeight={1}
-                    bgGradient="linear(to-l, #0ea5e9,#2563eb)"
-                    _hover={{ bgGradient: 'linear(to-l, #0ea5e9,#2563eb)' }}>
-                </Link>
-                    Create Projects
-                </Button> */}
-            {<Button
-
-            mt='30px'
-             as={Link}
-            href='signup'
-              rounded={'full'}
-              bg={'blue.400'}
-              color={'white'}
-              _hover={{ bgGradient: 'linear(to-l, #0ea5e9,#2563eb)'
-                
-              }}>
-              Create Projects
-            </Button> }
-            {/* {/<Button rounded={'full'}>How It Works</Button> } */}
           </Stack>
         </Stack>
       </Flex>
       <Flex mt='0px'flex={1}>
         <Image
         mt='60px'
-       
           alt={'Login Image'}
           objectFit={'cover'}
           src={
